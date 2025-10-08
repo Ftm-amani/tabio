1728d821657de3480330cc779c5b6fe6acf49d63
ftm-amani <ftm.amani95@gmail.com>
2025-10-08 20:55:04 +0200

    feat: implement clock

--- script.js
+++ script.js
@@ -108,3 +108,19 @@ saveBtn.onclick = () => {
   visionUpload.value = "";
   modal.classList.remove("active");
 };
+
+// === CLOCK ==============
+function updateClock() {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+
+    document.getElementById('hours').textContent = hours;
+    document.getElementById('minutes').textContent = minutes;
+    document.getElementById('seconds').textContent = seconds;
+}
+
+updateClock();
+setInterval(updateClock, 1000);
+
